371e4cdafd143bb38b1d8a3decec035b441acfd2
Shreesh Kulkarni <sgkul2000@gmail.com>
2020-11-28 02:15:15 +0530

    Fix: updated post build script

--- post_build_script.sh
+++ post_build_script.sh
@@ -2,6 +2,9 @@ echo Replacing paths...
 sed -i.bak 's/href=\"\//href=\".\//g' dist/index.html
 sed -i.bak 's/src=\"\//src=\".\//g' dist/index.html
 sed -i.bak 's/srcset=\"\//srcset=\".\//g' dist/index.html
+sed -i.bak 's/href=\"\//href=\".\//g' dist/404.html
+sed -i.bak 's/src=\"\//src=\".\//g' dist/404.html
+sed -i.bak 's/srcset=\"\//srcset=\".\//g' dist/404.html
 echo Replaced successfully
 echo Copying sitemap..
 cp sitemap.xml dist/
